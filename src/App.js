ef6a1684612a30a130a1f0d4cd687d1fc9e4acc7
Vishnu Kumar <vermaji1891@gmail.com>
2023-09-22 22:58:20 +0530

    Component and pages setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import ExcerciseDetail from "./pages/ExcerciseDetail";
+import Footer from "./component/Footer";
 
 const App = () => {
 
     return (
-        <div>App</div>
+        <Box width="400px" sx={{width: {xl: '1488px'}}} m='auto'>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/excercise/:id" element={<ExcerciseDetail />}/>
+            </Routes>
+            <Footer />
+        </Box>
     )
 }
 export default App;
